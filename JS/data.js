8cb1eae4fe953aafa0a97979bc0be1e1e8b42fa4
Andalib Iftakher <108351152+anda22101@users.noreply.github.com>
2025-02-11 09:25:13 +0600

    Homepage 50% Done

---
+++ JS/data.js
@@ -0,0 +1,339 @@
+const product = {
+  "kitchen scale": {
+    name: "10kg/1g",
+    "Brand Name": "",
+    Type: "",
+    Wheel: "NO",
+    Price: 10.6,
+    Specification: [],
+    Category: "Kitchen Scale",
+    "Picture Path": "./images/kitchen/main.jpg",
+    "number sold": 10,
+  },
+
+  "body scale": {
+    name: "Body Scale",
+    "Brand Name": "",
+    Type: "",
+    Wheel: "NO",
+    Price: 2.71,
+    Specification: [
+      "উপাদান: টেম্পার্ড গ্লাস + প্লাস্টিক",
+      "ফাংশন: ওজন পরিমাপ, BMI ফাংশনসহ, ব্লুটুথ সংযোগ",
+      "একক: কেজি / পাউন্ড",
+      "ব্যাটারি: 2*AAA শুকনো ব্যাটারি",
+      "ক্ষমতা: 180kg/50g",
+      "স্কেলের আকার: 26*26*2.3 cm",
+      "ডিসপ্লে: LCD",
+      "প্যাকিং: ১০ পিস / কার্টন",
+      "মাত্রা: 58*35*36 cm",
+      "মোট ওজন / নিট ওজন: 15kg / 14kg",
+    ],
+    Category: "Body Scale",
+    "Picture Path": "./images/bodyscale/main.jpg",
+    "number sold": 12,
+  },
+
+  "20kg/1g": {
+    name: "10kg/1g",
+    "Brand Name": "",
+    Type: "",
+    Wheel: "NO",
+    Price: 10.6,
+    Specification: [],
+    Category: "",
+    "Picture Path": "./images/kitchen/main.jpg",
+    "number sold": 0,
+  },
+
+  "30kg/1g": {
+    name: "10kg/1g",
+    "Brand Name": "",
+    Type: "",
+    Wheel: "NO",
+    Price: 10.6,
+    Specification: [],
+    Category: "",
+    "Picture Path": "./images/kitchen/main.jpg",
+    "number sold": 0,
+  },
+
+  "40kg/1g": {
+    name: "40kg/1g",
+    "Brand Name": "topstar*",
+    Type: "",
+    Wheel: "NO",
+    Price: 13,
+    Specification: [
+      "উপাদান: প্লাস্টিক + SS কি-প্যাড + সমতল প্লেট, প্লেটের ওজন 420g, প্লাস্টিক ফিট লক",
+      "ফাংশন: ওজন পরিমাপ, মূল্য গণনা",
+      "একক: কেজি / গ্রাম",
+      "রঙ: কাস্টমাইজড",
+      "ব্যাটারি: 4V/4A (420g) লিড-অ্যাসিড ব্যাটারি লক সিস্টেম সহ",
+      "ক্ষমতা: 40kg/1g",
+      "লোডসেল: 88mm",
+      "মাদারবোর্ড: কাস্টমাইজড ক্যালিব্রেশন কোড করা যাবে",
+      "স্কেলের আকার: 33*33*12.5 cm",
+      "ডিসপ্লে: সবুজ LED",
+      "লোডসেল স্ট্যান্ড: 工 আই-আকৃতির কাঠামো",
+      "প্যাকিং: ৫ পিস / কার্টন",
+      "মাত্রা: 65*39.5*37.5 cm",
+      "মোট ওজন / নিট ওজন: 15.5kg / 14kg",
+      "কোনো হ্যান্ডেল নেই",
+      "একই ব্র্যান্ড নাম থাকবে",
+      "রঙিন কার্টুন ডিজাইন পরে ইমেইলে প্রদান করা হবে",
+    ],
+    Category: "Kitchen Scale",
+    "Picture Path": "./images/40kg/main.png",
+    "number sold": 0,
+  },
+
+  "60kg/5g": {
+    name: "60kg/5g",
+    "Brand Name": "topstar*",
+    Type: "",
+    Wheel: "NO",
+    Price: 18.5,
+    Specification: [
+      "৬০ কেজি প্ল্যাটফর্ম স্কেল, ৪টি পিস এক মাস্টার কার্টনে",
+      "আকার: 30*34cm",
+      "ব্যাটারি: 4V 4A 450g",
+      "স্টিল কী ডিজিটাল ডুয়াল ডিসপ্লে",
+      "স্টেইনলেস স্টিল প্লেট",
+      "১৮০ কেজি সেন্সর প্যাকেজ",
+      "রঙ: সিলভার",
+      "বড় আকারের স্কেল ফুট",
+      "মাত্রা: 58*32.5*68cm",
+      "মোট ওজন / নিট ওজন: 23.2kg / 20.8kg",
+      "এক মাস্টার কার্টনে ৪টি পিস থাকবে",
+      "বডির ছিদ্র প্লাস্টিক কভার দিয়ে ঢেকে দিতে হবে",
+      "কালো রঙের ফুট ব্যবহার করতে হবে",
+      "মনিটর স্ট্যান্ডের প্ল্যাটফর্ম স্কেলে অবশ্যই একটি লোগো লিখতে হবে",
+    ],
+    Category: "Platform Scale",
+    "Picture Path": "./images/60kg/main.png",
+    "number sold": 0,
+  },
+
+  "100kg/5g": {
+    name: "100kg/5g",
+    "Brand Name": "GRAMEEN ROCKET",
+    Type: "SS",
+    Wheel: "NO",
+    Price: 10,
+    Specification: [
+      "উপাদান: লোহা স্ট্যান্ড + এসএস প্যান + এসএস কিপ্যাড ডিসপ্লে + বেড়া",
+      "এসএস প্যান আকার: ৩০*৪০ সেমি",
+      "ধারণক্ষমতা: ১০০ কেজি/১০ গ্রাম, এলইডি ডুয়াল ডিসপ্লে",
+      "২০০ কেজি বড় লোড সেল কভারের সঙ্গে",
+      "ব্যাটারি: ৪ভি ৪এ ৪২০ গ্রাম, তিন-পিন চার্জ, এসি ১১০ভি থেকে ২৪০ভি, ৯৯ বার একসুমুলেশন",
+      "প্রেস বোতাম: ৩০০,০০০ বার থেকে বেশি",
+      "মাদার বোর্ড: ডাবল এপোক্সি বোর্ড",
+      "ডিসপ্লে এবং কিপ্যাড: এপোক্সি বোর্ড",
+      "প্যাকিং: ৭০*৩১.৫*১৬ সেমি/১ পিসি",
+      "গ্রস ওজন/নেট ওজন: ৯.২২ কেজি/ ৯.১ কেজি",
+      "নাম: গ্রামীণ রকেট",
+    ],
+    Category: "Platform Scale",
+    "Picture Path": "./images/99kg/main.png",
+    "number sold": 100,
+  },
+
+  "100kg/10gMS": {
+    name: "10kg/1g",
+    "Brand Name": "",
+    Type: "MS",
+    Wheel: "NO",
+    Price: 23,
+    Specification: [],
+    Category: "Platform Scale",
+    "Picture Path": "./images/40kg/main.png",
+    "number sold": 0,
+  },
+
+  "100kg/10gSS": {
+    name: "100kg/10g SS",
+    "Brand Name": "GRAMEEN ROCKET",
+    Type: "SS",
+    Wheel: "NO",
+    Price: 10,
+    Specification: [
+      "মনিটরের সামনের এবং পিছনের ডিসপ্লের উজ্জ্বলতা একই হওয়া উচিত।",
+      "*১০০ কেজি/১০ গ্রাম প্ল্যাটফর্ম স্কেল রেল এবং পা সহ",
+      "*আকার: ৩৫*৪৫ সেমি",
+      "*রঙ - এসএস রঙ যেমন ছবি, কালো রাবার",
+      "*২০০ কেজি ধারণক্ষমতা লোড সেল কভার সহ",
+      "*৪ভি ৪এহ ৪৫০গ্রাম ব্যাটারি .৩ ক্যাবল",
+      "*মনিটর / ইনডিকেটর: যেমনটি আমি আপনাকে কিনেছিলাম, স্টিল কিপ্যাড",
+      "*ওজন পরিমাপ প্যান: (যেমন ৪০ X ৫০ সাইজের এসএস পণ্য, পা সহ যা আমি আপনাকে কিনেছিলাম)",
+      "*২০০ কেজি সেন্সর প্যাকেজ",
+      "*মেইন বোর্ড: ক্যালিব্রেশন কোড (যেমন ৪০ X ৫০ সাইজের এসএস পণ্য, পা সহ যা আমি আপনাকে কিনেছিলাম)",
+      "গ্রস ওজন/নেট ওজন: ১২ কেজি/১৩ কেজি",
+      "ব্র্যান্ড নাম: গ্রামীণ রকেট",
+      "নীল মার্ক করা স্থানে জাপান স্ট্যান্ডার্ড লিখতে হবে",
+    ],
+    Category: "Platform Scale",
+    "Picture Path": "./images/100kgss/main.png",
+    "number sold": 120,
+  },
+
+  "150kg/10gMS": {
+    name: "100kg/10g SS",
+    "Brand Name": "",
+    Type: "MS",
+    Wheel: "",
+    Price: 10,
+    Specification: [],
+    Category: "",
+    "Picture Path": "./images/40kg/main.png",
+    "number sold": 10,
+  },
+  "150kg/10gSS": {
+    name: "100kg/10g SS",
+    "Brand Name": "",
+    Type: "SS",
+    Wheel: "",
+    Price: 10,
+    Specification: [],
+    Category: "",
+    "Picture Path": "./images/40kg/main.png",
+    "number sold": 10000,
+  },
+
+  "250kg/20gMS": {
+    name: "100kg/10g SS",
+    "Brand Name": "",
+    Type: "MS",
+    Wheel: "",
+    Price: 10,
+    Specification: [],
+    Category: "",
+    "Picture Path": "./images/40kg/main.png",
+    "number sold": 10,
+  },
+  "250kg/20gSS": {
+    name: "100kg/10g SS",
+    "Brand Name": "",
+    Type: "SS",
+    Wheel: "",
+    Price: 10,
+    Specification: [],
+    Category: "",
+    "Picture Path": "./images/40kg/main.png",
+    "number sold": 10,
+  },
+
+  "300kg/20gMS": {
+    name: "300kg/20g MS",
+    "Brand Name": "GRAMEEN ROCKET",
+    Type: "MS",
+    Wheel: "YES",
+    Price: 10,
+    Specification: [
+      "*৩০০ কেজি প্ল্যাটফর্ম স্কেল",
+      "*আকার: ৪৫*৬০ সেমি",
+      "*৪ভি ৪এ ৪৫০গ্রাম ব্যাটারি",
+      "*স্টিল কিপ্যাড ডিজিটাল ডুয়াল ডিসপ্লে",
+      "*ওজন পরিমাপ প্যানের পুরুত্ব ১.৮ মিমি",
+      "*আই-আকৃতির বেড়ার উচ্চতা ৬৫ সেমি",
+      "*মাথার উচ্চতা ৯১ সেমি",
+      "*৮০০ কেজি সেন্সর প্যাকেজ",
+      "*রঙিন ছবি মডেল",
+      "*চাকা সহ",
+      "আকার: ৯৯*৪৬*২৩.৫ সেমি",
+      "গ্রস ওজন/নেট ওজন: ২৬ কেজি/২৫ কেজি",
+    ],
+    Category: "Platform Scale",
+    "Picture Path": "./images/300kgms/main.png",
+    "number sold": 1000,
+  },
+
+  "500kg/50gMS": {
+    name: "100kg/10g SS",
+    "Brand Name": "",
+    Type: "MS",
+    Wheel: "YES",
+    Price: 10,
+    Specification: [],
+    Category: "",
+    "Picture Path": "./images/40kg/main.png",
+    "number sold": 10,
+  },
+  "500kg/50gSS": {
+    name: "100kg/10g SS",
+    "Brand Name": "",
+    Type: "SS",
+    Wheel: "NO",
+    Price: 10,
+    Specification: [],
+    Category: "",
+    "Picture Path": "./images/40kg/main.png",
+    "number sold": 10,
+  },
+};
+
+let query = [];
+
+for (let key in product) {
+  let value = product[key]["number sold"];
+  
+  if (query.length == 0) query.push(key);
+  
+    
+  else if (query.length == 1){
+    if (value > product[query[0]]["number sold"]) query.unshift(key);
+
+    else query.push(key)
+  }
+
+
+  else if (query.length == 2){
+    if (value > product[query[0]]["number sold"]) query.unshift(key);
+    
+    else if (value > product[query[1]]["number sold"]) query.splice(1,0,key);
+
+    else query.push(key);
+  }
+
+
+  else if (query.length == 3){
+    if (value > product[query[0]]["number sold"]) {
+      query.unshift(key);
+      query.pop();
+    }
+    
+    else if (value > product[query[1]]["number sold"]){ 
+      query.splice(1,0,key);
+      query.pop();
+    }
+    
+    else if (value > product[query[2]]["number sold"]){ 
+      query.splice(2,0,key);
+      query.pop();
+    }
+  }}
+
+console.log(query);
+
+
+
+
+
+
+let productGrid = document.getElementsByClassName("product-grid")[0];
+
+function displayFeaturedProd() {
+  query.forEach((productKey) => {
+    let currentProduct = product[productKey];
+    let productCard = document.createElement("a");
+    productCard.href = "#";
+    productCard.innerHTML = `<div class="product-card">
+        <img src="${currentProduct["Picture Path"]}" alt="${productKey}" />
+        <h2>${currentProduct["name"]}</h3>
+        <h3>বিক্রিত সংখ্যা: ${currentProduct["number sold"]}<h3>
+      </div>`;
+
+    productGrid.appendChild(productCard);
+  });
+}
+displayFeaturedProd();
