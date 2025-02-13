176929a3103ecc83d550d1b23f5e1e104e5a483a
Andalib Iftakher <108351152+anda22101@users.noreply.github.com>
2025-02-14 00:24:26 +0600

    search product fix

--- JS/allProd.js
+++ JS/allProd.js
@@ -92,7 +92,7 @@ function displayFeaturedProd(query) {
   query.forEach((productKey) => {
     let currentProduct = product[productKey];
     let productCard = document.createElement("a");
-    productCard.href = '#';
+    productCard.href = `../html/product_detail.html?productKey=${productKey}`;
     productCard.innerHTML = `<div class="product-card">
           <img src=".${currentProduct["Picture Path"]}" alt="${productKey}" />
           <h2>${currentProduct["name"]}</h2>
